9565a8656ae4b627e0d229f1ee2344a4105dde91
Darlan Mendonça <darlanmendonca@gmail.com>
2016-09-28 22:53:08 -0300

    fix comma in bs task

--- tasks/browser-sync.js
+++ tasks/browser-sync.js
@@ -1,14 +1,14 @@
 import gulp from 'gulp';
-import config from './gulp.config.js';
+import {browserSync, browserSyncOptions} from './gulp.config.js';
 import historyApi from 'connect-history-api-fallback';
 import gzip from 'compression';
 
 gulp.task('browser-sync', browserSyncTask);
 
 function browserSyncTask() {
-  config.browserSyncOptions.middleware = [
+  browserSyncOptions.middleware = [
     historyApi(),
     gzip(),
   ];
-  config.browserSync.init(config.browserSyncOptions);
+  browserSync.init(browserSyncOptions);
 }
