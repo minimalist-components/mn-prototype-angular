9594f09d3bac3eda3010c46bd10813c23787f794
Darlan Mendonça <darlanmendonca@gmail.com>
2016-09-28 22:44:22 -0300

    add gzip to browser-sync server

--- tasks/browser-sync.js
+++ tasks/browser-sync.js
@@ -1,8 +1,14 @@
 import gulp from 'gulp';
 import config from './gulp.config.js';
+import historyApi from 'connect-history-api-fallback';
+import gzip from 'compression';
 
 gulp.task('browser-sync', browserSyncTask);
 
 function browserSyncTask() {
+  config.browserSyncOptions.middleware = [
+    historyApi(),
+    gzip(),
+  ];
   config.browserSync.init(config.browserSyncOptions);
 }
