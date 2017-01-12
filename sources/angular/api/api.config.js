db3c143453965ae233325ca3f7d91f4c82c422a9
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-12 15:11:10 -0200

    adjust update status code to 200

--- sources/angular/api/api.config.js
+++ sources/angular/api/api.config.js
@@ -5,4 +5,12 @@ angular
 function ApiConfig(RestangularProvider) {
   RestangularProvider.setBaseUrl('http://localhost:4000')
   RestangularProvider.setRestangularFields({id: '_id'})
+  RestangularProvider.addResponseInterceptor(test)
+
+  function test(data, operation) { // what, url, response, deferred
+    if (operation === 'save') {
+      console.log(operation)
+    }
+    return data
+  }
 }
