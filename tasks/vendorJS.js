3a44371003695809ec5bc4234422c891c2259000
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-25 16:55:51 -0200

    tests with package-files

--- tasks/vendorJS.js
+++ tasks/vendorJS.js
@@ -2,10 +2,15 @@ import gulp from 'gulp'
 import bowerFiles from 'bower-files'
 import concat from 'gulp-concat'
 import uglify from 'gulp-uglify'
+// import packageFiles from 'package-files'
 
 gulp.task('vendorJS', vendorJSTask)
 
 function vendorJSTask() {
+  // const packageDependencies = packageFiles().filter(file => file.endsWith('.css'))
+  // console.log(packageDependencies)
+  // return false
+
   const dependencies = bowerFiles()
     .ext('js')
     .files
