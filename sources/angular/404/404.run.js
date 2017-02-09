44f6805dd344f40ec5c4cf3bb1ee259119a5870a
Darlan Mendonça <darlanmendonca@gmail.com>
2017-02-09 20:07:55 -0200

    fix and display 404 page only for status 404

--- sources/angular/404/404.run.js
+++ sources/angular/404/404.run.js
@@ -5,7 +5,9 @@ angular
 function NotFoundRun($rootScope, $state) {
   $rootScope.$on('$stateChangeError', redirectTo404)
 
-  function redirectTo404() {
-    $state.go('404')
+  function redirectTo404(event, toState, toParams, fromState, fromParams, error) {
+    if (error.status === 404) {
+      $state.go('404')
+    }
   }
 }
