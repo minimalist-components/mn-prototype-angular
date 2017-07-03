1e608af7cd581ae2d916664c268747b1313605f4
Darlan Mendonça <darlanmendonca@gmail.com>
2017-07-03 13:53:16 -0300

    comment chai-test-cafe to continue later

--- sources/angular/login/login.spec.js
+++ sources/angular/login/login.spec.js
@@ -1,6 +1,6 @@
 const PageObject = require('./login.po.js')
 const {expect} = require('chai')
-  .use(require('../../../chai-test-cafe.js'))
+  // .use(require('../../../chai-test-cafe.js'))
   .use(require('chai-as-promised'))
 
 let login
@@ -12,28 +12,39 @@ fixture `login`
   })
 
   test('has a email input', async () => {
-    await expect(login.email).to.exists
+    await expect(login.email.exists).to.eventually.be.true
+    // await expect(login.email).to.exists
   })
 
   test('has a password input', async () => {
-    await expect(login.password).to.exists
+    await expect(login.password.exists).to.eventually.be.true
+    // await expect(login.password).to.exists
   })
 
   test('has a submit button', async () => {
-    await expect(login.button).to.exists
+    await expect(login.button.exists).to.eventually.be.true
+    // await expect(login.button).to.exists
   })
 
   test('has a message hidden (wrong credentials)', async () => {
-    await expect(login.message).to.exists
-    await expect(login.message).to.have.text('The email or password you’ve entered doesn’t match any account')
-    await expect(login.message).to.not.have.class('visible')
+    await expect(login.message.exists).to.eventually.be.true
+    await expect(login.message.innerText).to.eventually.be.equal('The email or password you’ve entered doesn’t match any account')
+    // await expect(login.message).to.exists
+    // await expect(login.message).to.have.text('The email or password you’ve entered doesn’t match any account')
   })
 
-  test('submit empty form, show invalid fields', async () => {
+  test('submit empty form, show email as required field', async () => {
     await login.submit()
-    await expect(login.email).to.have.class('invalid')
-    await expect(login.password).to.have.class('invalid')
-    await expect(login.message).to.not.have.class('visible')
+    await expect(login.email.hasClass('invalid')).to.eventually.be.true
+    await expect(login.email.hasClass('required')).to.eventually.be.true
+    // await expect(login.email).to.have.class('.invalid.required')
+  })
+
+  test('submit empty form, show password as required field', async () => {
+    await login.submit()
+    await expect(login.password.hasClass('invalid')).to.eventually.be.true
+    await expect(login.password.hasClass('required')).to.eventually.be.true
+    // await expect(login.password).to.have.class('.invalid.required')
   })
 
   test('show message when try to login with invalid credentials', async (page) => {
@@ -41,7 +52,8 @@ fixture `login`
     await login.submit()
     const {pathname} = await page.eval(() => window.location)
 
-    await expect(login.message).to.have.class('visible')
+    await expect(login.message.hasClass('visible')).to.eventually.be.true
+    // await expect(login.message).to.have.class('visible')
     await expect(pathname).to.equal('/login')
   })
 
